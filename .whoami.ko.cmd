7f0d4f1e8fcc0149abf01165e0ede78667e126c4
Marco Flores <92607298+2Esio@users.noreply.github.com>
2026-02-20 00:21:25 -0600

    Delete .whoami.ko.cmd

--- .whoami.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/marco/Documents/kmods/whoami.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T scripts/module.lds -o /home/marco/Documents/kmods/whoami.ko /home/marco/Documents/kmods/whoami.o /home/marco/Documents/kmods/whoami.mod.o
